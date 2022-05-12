4d3baaacf76279613cbad937d4f228013d0bbd92
马二十 <3133169343@qq.com>
2022-05-12 19:16:18 +0800

    2022-5-12

--- 03/03.cpp
+++ 03/03.cpp
@@ -1,36 +1,55 @@
-#include <stdio.h>
-#include <string.h>
-char s[201];
+#include <bits/stdc++.h>
+
+using namespace std;
+
 int main()
 {
-    int i, len, hl, f;
-    scanf("%s", s);
+    ios::sync_with_stdio(false);
+    string s;
+    cin >> s;
 
-    len = strlen(s) - 1;
-    hl = (len + 1) >> 1;
-    for (i = hl - 1, f = 0; i >= 0 && !f; i--)
-        f = s[i] == s[len - i] ? 0 : (s[i] > s[len - i] ? 1 : -1);
-    if (f > 0)
+    while (s.size() > 0 && s[0] == '0') //去除前导0 000006576->6576
     {
-        s[len >> 1]--;
-        for (i = len >> 1; i && s[i] > '9'; i--)
-        {
-            s[i] = '0';
-            s[i - 1]--;
-        }
-        if (s[0] > '9')
-        {
-            s[0] = '1';
-            len++;
-            hl = (len + 1) >> 1;
-            s[hl] = '0';
-        }
+        s.erase(0, 1); //从S中删除迭代器对0和1所表示的范围中的元素，返回1的元素
     }
-    for (i = 0; i < hl; i++)
-        putchar(s[i]);
-    for (i = hl - (len & 1); i >= 0; i--)
-        putchar(s[i]);
-    // putchar(10);
+    // cout << s << endl;
+    if (s.size() == 0) // 0 并结束
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+    else if (s.size() == 1) // 个位数 并结束
+    {
+        cout << s << endl;
+        return 0;
+    }
+
+    int len = s.length() / 2;
+    for (int i = 0; i < len; i++)
+    {
+        int j = s.length() - i - 1;
+        if (s[i] > s[j])
+        {
+            /* 前借位 */
+            int k = j - 1;
+            while (k >= 0 && s[k] == '0')
+            {
+                s[k--] = '9'; // k位置9 && k--
+            }
+            s[k]--; //前一位减1
+            if (k == 0 && s[k] == 0)
+            {
+                s[i] = '9';
+                s.erase(0, 1);
+                // len = s.length() / 2;
+                s[s.length()-1]='9';
+                // i = -1;
+                break;;
+            }
+        }
+        s[j] = s[i];
+    }
+    cout << s << endl;
 
     return 0;
 }
